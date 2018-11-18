99a2aed49dc7e5ed4f35f8d2a9f4d507a8e514cb
Heng2j <heng2j@gmail.com>
2018-11-18 10:13:29 -0500

    Start Pytest and TDDed on get_one_random from synonym module

--- src/synonym.py
+++ src/synonym.py
@@ -3,7 +3,7 @@
 # ---------------
 # Author: Zhongheng Li
 # Start Date: 11-17-18
-# Last Modified Date: 11-17-18
+# Last Modified Date: 11-18-18
 
 
 """
@@ -14,36 +14,47 @@ synonym collection
 
 
 # System modules
-import requests
+import random
 
 # 3rd party modules
+import requests
 from flask import make_response, abort
 
+
+def get_synonyms_from_datamuse(word_list, max_num=20 , min_interpreting_score=25000 ):
+
+    """
+    :param word_list: A list of string to query the datamuse Means like(ml) endpoint for synonyms
+    :param max_num: Max return result. Set to 20 by default
+    :param min_interpreting_score: Minimum interpreting score to select the most relevant synonyms. Set to 25000 by default
+    :return: A list of synonyms where the interpreting score is higher than the min_interpreting_score
+    """
+
+    datamuse_ml_api_url = 'https://api.datamuse.com/words?ml='
+
+    values = "+".join([word for word in word_list])
+
+    max_num_str = '&max=' + str(max_num)
+
+    response = requests.get(datamuse_ml_api_url + values + max_num_str)
+
+    if (response.status_code != 200) or (len(response.text) == 0):
+
+        abort(
+            404, "Synonyms can not be found with the given phase: {word_list}".format(word_list=word_list)
+        )
+
+
+    return [ word_item['word'] for word_item in response.json() if word_item['score'] > min_interpreting_score]
 
 
 def get_one_random(word_list):
 
-
-
     """
-    This function
-
-    This function responds to a request for /api/people
-    with the complete lists of people
-
-    :return:        json string of list of people
+    :param word_list: A list of string to query the datamuse Means like(ml) endpoint for synonyms
+    :return: A random choice from the synonyms that returned from get_synonyms_from_datamuse
     """
-    # Create the list of people from our data
 
+    synonyms = get_synonyms_from_datamuse(word_list, max_num=20)
 
-    requests = ""
-
-    resp = requests.get('https://todolist.example.com/tasks/')
-    if resp.status_code != 200:
-        # This means something went wrong.
-        raise ApiError('GET /tasks/ {}'.format(resp.status_code))
-    for todo_item in resp.json():
-
-
-
-    return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
+    return random.choice(synonyms)
