1dcb83158095cf2f3a2d4709ae0a0d5203c7835e
Heng2j <heng2j@gmail.com>
2018-11-19 18:01:57 -0500

    Updated parse_data_set in MetroLyrics_data to save data SQLite

--- src/data_tools/MetroLyrics_data.py
+++ src/data_tools/MetroLyrics_data.py
@@ -17,12 +17,19 @@ https://www.kaggle.com/gyani95/380000-lyrics-from-metrolyrics
 """
 
 # System modules
+import os
+from os.path import dirname as up
 
 # 3rd party modules
 import pandas as pd
 
 # Internal modules
 from config import db
+
+from sqlalchemy import create_engine
+
+
+basedir = up(os.path.abspath(os.path.dirname(__file__)))
 
 
 def parse_data_set(data_set_path):
@@ -36,4 +43,11 @@ def parse_data_set(data_set_path):
 
     data_df = pd.read_csv(data_set_path)
 
-    data_df.to_sql(name='songs', con=db.get_engine(), if_exists='append', index=False)
+    # Build the Sqlite ULR for SqlAlchemy
+    sqlite_url = "sqlite:////" + os.path.join(basedir, "story_writer.db")
+
+    print("sqlite_url: ", sqlite_url)
+
+    engine = create_engine(sqlite_url, echo=False)
+
+    data_df.to_sql(name='songs', con=engine, if_exists='append', index=False)
